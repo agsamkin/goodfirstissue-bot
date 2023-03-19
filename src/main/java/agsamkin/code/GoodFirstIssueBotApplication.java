db4d1eed44be718f88c8f6fda31c88d0f664623a
ags <a.g.samkin@gmail.com>
2023-03-19 19:43:21 +0300

    Initial commit

---
+++ src/main/java/agsamkin/code/GoodFirstIssueBotApplication.java
@@ -0,0 +1,13 @@
+package agsamkin.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodFirstIssueBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodFirstIssueBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agsamkin/code/GoodFirstIssueBotApplicationTests.java
@@ -0,0 +1,13 @@
+package agsamkin.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodFirstIssueBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

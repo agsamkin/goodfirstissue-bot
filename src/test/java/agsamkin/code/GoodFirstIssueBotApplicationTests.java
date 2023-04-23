2e5ff8500a6ad1e87fb488041bf64b0f3699d559
ags <a.g.samkin@gmail.com>
2023-04-23 13:34:13 +0300

    Fix test config

--- src/test/java/agsamkin/code/GoodFirstIssueBotApplicationTests.java
+++ src/test/java/agsamkin/code/GoodFirstIssueBotApplicationTests.java
@@ -1,8 +1,10 @@
 package agsamkin.code;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class GoodFirstIssueBotApplicationTests {
     @Test
---
+++ src/test/java/agsamkin/code/config/SpringConfigForIT.java
@@ -0,0 +1,24 @@
+package agsamkin.code.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@Profile(SpringConfigForIT.TEST_PROFILE)
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "hexlet.code")
+@PropertySource(value = "classpath:/config/application.yml")
+public class SpringConfigForIT {
+    public static final String TEST_PROFILE = "test";
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+}

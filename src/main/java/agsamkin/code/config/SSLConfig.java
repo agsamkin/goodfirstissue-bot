68063fd481e353963b13dce8fee50ec836501781
ags <a.g.samkin@gmail.com>
2023-04-28 20:28:13 +0300

    Fix ssl config

--- src/main/java/agsamkin/code/config/SSLConfig.java
+++ src/main/java/agsamkin/code/config/SSLConfig.java
@@ -5,21 +5,24 @@ import org.springframework.boot.web.server.Ssl;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile(value = "prod")
 @Configuration
 public class SSLConfig {
     @Bean
     public ConfigurableServletWebServerFactory webServerFactory() throws Exception {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
         Ssl ssl = new Ssl();
         ssl.setEnabled(true);
-        ssl.setCertificate("YOURPUBLIC.pem");
-        ssl.setCertificatePrivateKey("YOURPRIVATE.key");
+        ssl.setCertificate("ssl/public.pem");
+        ssl.setCertificatePrivateKey("ssl/private.key");
         ssl.setKeyStoreType("PKCS12");
         ssl.setKeyStorePassword("");
+
         factory.setSsl(ssl);
         factory.setPort(443);
-
         return factory;
     }
 }
